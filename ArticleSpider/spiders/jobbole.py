9f06d655026a9348d4eb1bd633772acf416d2617
FrozenLin <linchnx@163.com>
2018-04-06 22:48:39 +0800

    收集jobbole404页面

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -18,15 +18,25 @@ class JobboleSpider(scrapy.Spider):
     allowed_domains = ['blog.jobbole.com']
     start_urls = ['http://blog.jobbole.com/all-posts/']
 
-    def __init__(self):
-        self.browser = webdriver.Chrome(executable_path="E:/Software/chromedriver_win32/chromedriver.exe")
-        super(JobboleSpider, self).__init__()
-        dispatcher.connect(self.spider_closed, signals.spider_closed)
+    # def __init__(self):
+    #     self.browser = webdriver.Chrome(executable_path="E:/Software/chromedriver_win32/chromedriver.exe")
+    #     super(JobboleSpider, self).__init__()
+    #     dispatcher.connect(self.spider_closed, signals.spider_closed)
+    #
+    # def spider_closed(self, spider):
+    #     # 当爬虫退出的时候关闭Chrome
+    #     print("spider closed")
+    #     self.browser.quit()
 
-    def spider_closed(self, spider):
-        # 当爬虫退出的时候关闭Chrome
-        print("spider closed")
-        self.browser.quit()
+    # 收集伯乐在线所有404的url以及404页面数
+    handle_httpstatus_list = [404]
+
+    def __init__(self, **kwargs):
+        self.fail_urls = []
+        dispatcher.connect(self.handle_spider_closed, signals.spider_closed)
+
+    def handle_spider_closed(self, spider, reason):
+        self.crawler.stats.set_value("failed_urls", ",".join(self.fail_urls))
 
     def parse(self, response):
         """
@@ -34,6 +44,10 @@ class JobboleSpider(scrapy.Spider):
         2. 获取下一页的url并交给scrapy进行下载，下载完成后交给parse
         """
         # 解析列表页中的所有文章url并交给scrapy下载后并进行解析
+        if response.status == 404:
+            self.fail_urls.append(response.url)
+            self.crawler.stats.inc_value("failed_url")
+
         post_nodes = response.css("#archive .floated-thumb .post-thumb a")
         for post_node in post_nodes:
             image_url = post_node.css("img::attr(src)").extract_first("")
